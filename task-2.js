8d344e5be98492451113ed310b19430040ca2ae2
sekar004845 <sekar004845@gmail.com>
2023-02-06 01:46:34 -0800

    update

---
+++ task-2.js
@@ -0,0 +1,94 @@
+//Addition//
+var a=5;
+var b=5;
+var c =5;
+var d=a+b+c;
+console.log(d);
+
+//Subtraction//
+var a=10;
+var b=5;
+var c=a-b;
+console.log(c);
+
+//Multiplication//
+var a=10;
+var b=5;
+var c=a*b;
+console.log(c);
+
+//Division//
+var a=10;
+var b=5;
+var c=a/b;
+console.log(c);
+
+//Area Of Circle//
+var r=7;
+var area=22/7*r**2;
+console.log(area);
+
+//Triangle//
+var a=7;
+var b=2;
+var area=1/2*a*b;
+console.log(area);
+
+//Simple Interest
+var a=10000;
+var b=10;
+var c=1;
+var SI=a*b*c/100;
+console.log(SI);
+
+//Square//
+var a=2;
+var area =a**2;
+console.log(area);
+
+//cube//
+var a=2;
+var area = 6**2;
+console.log(area);
+
+//Average//
+var a=5;
+var b=5;
+var c=5;
+var avg=a+b+c/3;
+console.log(avg);
+
+//Prime or Not//
+var a=983;
+var b=a%10;
+var c=a%100;
+var pr=(b+(c)-b%10)+(a-c%100);
+console.log(pr==0);
+
+//larger number//
+var a=5;
+var b=10;
+var c=a>b;
+console.log(c);
+
+
+//Positive or Not//
+var a=5;
+var b=a>0;
+console.log(b);
+
+//Greater than 10//
+var a=11;
+var b=a>10;
+console.log(b);
+
+//Even or Odd//
+
+
+//Divisible by 5//
+var a=100;
+var b=100%5;
+console.log(b==0);
+
+
+
